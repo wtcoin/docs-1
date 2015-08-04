ef0f34adef51ff7fd403ee62422a42f4b87e149c
Josh Cincinnati <acityinohio@users.noreply.github.com>
2015-08-04 21:47:34 +0000

    more cleanup of base index and quick websocket js fix

--- source/javascripts/app/_websocket-browser.js
+++ source/javascripts/app/_websocket-browser.js
@@ -1,20 +1,18 @@
-(function (global) {
-	$(document).ready(function() {
-	$('button.tryme').click(function(e) {
-	var ws = new WebSocket("wss://socket.blockcypher.com/v1/btc/main");
-	var count = 0;
-	ws.onmessage = function (event) {
-		var tx = JSON.parse(event.data);
-		var shortHash = tx.hash.substring(0, 6) + "...";
-		var total = tx.total / 100000000;
-		var addrs = tx.addresses.join(", ");
-		$('#browser-websocket').before("<div>Unconfirmed transaction " + shortHash + " totalling " + total + "BTC involving addresses " + addrs + "</div>");
-		count++;
-		if (count > 10) ws.close();
-	}
-	ws.onopen = function(event) {
-		ws.send(JSON.stringify({filter: "event=unconfirmed-tx"}));
-	}
-	});
-	});
-})(window);
+$(document).ready(function() {
+$('button.tryme').click(function(e) {
+var ws = new WebSocket("wss://socket.blockcypher.com/v1/btc/main");
+var count = 0;
+ws.onmessage = function (event) {
+	var tx = JSON.parse(event.data);
+	var shortHash = tx.hash.substring(0, 6) + "...";
+	var total = tx.total / 100000000;
+	var addrs = tx.addresses.join(", ");
+	$('#browser-websocket').before("<div>Unconfirmed transaction " + shortHash + " totalling " + total + "BTC involving addresses " + addrs + "</div>");
+	count++;
+	if (count > 10) ws.close();
+}
+ws.onopen = function(event) {
+	ws.send(JSON.stringify({filter: "event=unconfirmed-tx"}));
+}
+});
+});
